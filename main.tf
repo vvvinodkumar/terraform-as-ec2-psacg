8d94b89680cd7adde3fabac30f431643379722c0
vvvinodkumar <vinodkumar.rgm@gmail.com>
2024-07-16 09:36:00 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "inst" {
+  count          = var.instance_count
+  ami            = var.ami
+  instance_type  = var.instance_type
+  subnet_id      = var.subnet
+
+  tags = {
+    Name = "TERRAFORM-GURU-${count.index}"
+  }
+}
+
+variable "instance_count" {
+  type        = string
+  default     = 2
+}
+
+variable "ami" {
+}
+
+variable "instance_type" {
+}
+
+variable "subnet" {
+}
+
+output "aws_instances" {
+  value = aws_instance.inst.*.tags.Name
+}
